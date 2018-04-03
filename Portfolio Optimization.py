b9ec4c7ec07b40d01887ec5183399db8c130b475
kellen0128 <kellen0128@gmail.com>
2018-04-03 16:32:55 -0400

    Update Portfolio Optimization.py

--- Portfolio Optimization.py
+++ Portfolio Optimization.py
@@ -44,11 +44,124 @@ log_ret.cov()
 log_ret.cov()*252 # annualized cov: multiply by days   
 
 
+#### Single Run for some random allocation
+# Set seed (optional)
+np.random.seed(101)
+
+# Stock Columns
+print('Stocks')
+print(stocks.columns)
+print('\n')
+
+# Create Random Weights
+print('Creating Random Weights')
+weights = np.array(np.random.random(4))
+print(weights)
+print('\n')
+
+# Rebalance Weights
+print('Rebalance to sum to 1.0')
+weights = weights / np.sum(weights)
+print(weights)
+print('\n')
+
+# Expected Return
+print('Expected Portfolio Return')
+exp_ret = np.sum(log_ret.mean() * weights) *252
+print(exp_ret)
+print('\n')
+
+# Expected Variance
+print('Expected Volatility')
+exp_vol = np.sqrt(np.dot(weights.T, np.dot(log_ret.cov() * 252, weights)))
+print(exp_vol)
+print('\n')
+
+# Sharpe Ratio
+SR = exp_ret/exp_vol
+print('Sharpe Ratio')
+print(SR)
 
 
 
+### Creating as a function
+num_ports = 15000
 
+all_weights = np.zeros((num_ports,len(stocks.columns)))
+ret_arr = np.zeros(num_ports)
+vol_arr = np.zeros(num_ports)
+sharpe_arr = np.zeros(num_ports)
 
+for ind in range(num_ports):
+
+    # Create Random Weights
+    weights = np.array(np.random.random(4))
+
+    # Rebalance Weights
+    weights = weights / np.sum(weights)
+    
+    # Save Weights
+    all_weights[ind,:] = weights
+
+    # Expected Return
+    ret_arr[ind] = np.sum((log_ret.mean() * weights) *252)
+
+    # Expected Variance
+    vol_arr[ind] = np.sqrt(np.dot(weights.T, np.dot(log_ret.cov() * 252, weights)))
+
+    # Sharpe Ratio
+    sharpe_arr[ind] = ret_arr[ind]/vol_arr[ind]
+
+#### Plotting the data
+plt.figure(figsize=(12,8))
+plt.scatter(vol_arr,ret_arr,c=sharpe_arr,cmap='plasma')
+plt.colorbar(label='Sharpe Ratio')
+plt.xlabel('Volatility')
+plt.ylabel('Return')
+
+# Add red dot for max SR
+plt.scatter(max_sr_vol,max_sr_ret,c='red',s=50,edgecolors='black')
+
+#### Creating Optimization function
+#### functionalize return and SR operations
+def get_ret_vol_sr(weights):
+    """
+    Takes in weights, returns array or return,volatility, sharpe ratio
+    """
+    weights = np.array(weights)
+    ret = np.sum(log_ret.mean() * weights) * 252
+    vol = np.sqrt(np.dot(weights.T, np.dot(log_ret.cov() * 252, weights)))
+    sr = ret/vol
+    return np.array([ret,vol,sr])
+
+from scipy.optimize import minimize
+
+# Creating negative sharpe ratio(negative minimization)
+def neg_sharpe(weights):
+    return  get_ret_vol_sr(weights)[2] * -1
+  
+# Contraints
+def check_sum(weights):
+    '''
+    Returns 0 if sum of weights is 1.0
+    '''
+    return np.sum(weights) - 1
+  
+# By convention of minimize function it should be a function that returns zero for conditions
+cons = ({'type':'eq','fun': check_sum})
+  
+# 0-1 bounds for each weight
+bounds = ((0, 1), (0, 1), (0, 1), (0, 1))
+
+# Initial Guess (equal distribution)
+init_guess = [0.25,0.25,0.25,0.25]
+
+# Sequential Least SQuares Programming (SLSQP).
+opt_results = minimize(neg_sharpe,init_guess,method='SLSQP',bounds=bounds,constraints=cons)
+
+opt_results
+opt_results.x   ### sharp
+get_ret_vol_sr(opt_results.x)
 
 
 
